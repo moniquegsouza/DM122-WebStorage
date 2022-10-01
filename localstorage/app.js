0fba82bb81148f61446b164324b781c426fc02ca
moniquegsouza <monique.gabriela@pg.inatel.br>
2022-10-01 14:18:45 -0300

    feat: list all content from local storage

---
+++ localstorage/app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = {
+    key: form.key.value,
+    value: form.value.value,
+  };
+  console.log({ formData });
+  window.localStorage.setItem(form.key.value, form.value.value);
+  form.reset();
+  form.key.focus();
+  readFromStorage();
+});
+
+function readFromStorage() {
+  document.querySelector("output").innerHTML = Object.keys(window.localStorage)
+    .map(htmlTemplate)
+    .join("");
+}
+
+function htmlTemplate(key) {
+  const value = window.localStorage.getItem(key);
+  return `
+    <span>${key}</span>
+    <span>${value}</span>
+  `;
+}
+
+readFromStorage();
